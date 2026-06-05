345b6f539fd2f38f4e8f091b1be3d5e9158170fc
Pavel Ciorici <cioricip@gmail.com>
2026-06-05 14:18:27 +0200

    1.4.26

--- wpzoom-portfolio.php
+++ wpzoom-portfolio.php
@@ -14,7 +14,7 @@
  * Author:      WPZOOM
  * Author URI:  https://www.wpzoom.com
  * Text Domain: wpzoom-portfolio
- * Version:     1.4.25
+ * Version:     1.4.26
  * License:     GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
